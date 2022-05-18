8b342e076be5eb8334e1e7fe38f2decaa719b43f
Klemens <klemens.d.morgenstern@gmail.com>
2022-05-18 18:06:34 +0800

    Project skeleton.

---
+++ include/boost/xproc.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Samuel Venable
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BOOST_XPROC_XPROC_HPP
+#define BOOST_XPROC_XPROC_HPP
+
+#include <boost/xproc/config.hpp
+
+#endif //BOOST_XPROC_XPROC_HPP
---
+++ include/boost/xproc/config.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Klemens D. Morgenstern
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BOOST_XPROC_CONFIG_HPP
+#define BOOST_XPROC_CONFIG_HPP
+
+
+
+#endif //BOOST_XPROC_CONFIG_HPP
